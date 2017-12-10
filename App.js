84c3d3ad191097d3cabf81e0e0151f3e48e130d1
WillyLu326 <luzh0326@gmail.com>
2017-12-10 16:52:13 -0500

    eject project

--- App.js
+++ App.js
@@ -2,11 +2,21 @@ import React from 'react';
 import { StyleSheet, View, TextInput } from 'react-native';
 
 export default class App extends React.Component {
+  state = {
+    text: ''
+  }
+
+  textChangeHandler = value => {
+    alert(value);
+  }
+
   render() {
     return (
       <View style={styles.container}>
         <TextInput 
           style={ {width: 300} }
+          value={this.state.text}
+          onChangeText={this.textChangeHandler}
         />
 
       </View>
